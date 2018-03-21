0edeb46361440e8e874e60a6123fb396c806855f
Alexandr Fanamurov <fanamurov@ya.ru>
2018-03-21 18:03:38 +1000

    add renderAdminMenu

--- src/VscaleComponent.php
+++ src/VscaleComponent.php
@@ -2,6 +2,7 @@
 
 namespace Larrock\ComponentVscale;
 
+use Larrock\ComponentVscale\Facades\LarrockVscale;
 use Larrock\Core\Component;
 
 class VscaleComponent extends Component
@@ -11,5 +12,11 @@ class VscaleComponent extends Component
         $this->name = 'vscale';
         $this->title = 'Vscale API';
         $this->description = 'Мост к Vscale API';
+    }
+
+    public function renderAdminMenu()
+    {
+        return view('larrock::admin.sectionmenu.types.default',
+            ['app' => LarrockVscale::getConfig(), 'url' => '/admin/'. LarrockVscale::getName()]);
     }
 }
